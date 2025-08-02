5ceb4e3b6a28c20e1ba7eee46846ce2e2e9046a1
PavloKordunov <102924208+PavloKordunov@users.noreply.github.com>
2025-08-02 10:49:57 +0300

    update CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,25 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as bodyParser from 'body-parser';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as bodyParser from 'body-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  
-  app.setGlobalPrefix('api');
+    const app = await NestFactory.create(AppModule)
 
-  app.use(bodyParser.json({ limit: '50mb' }));
-  app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  
-  app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://math-train-frontend.onrender.com'
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
-  });
-  
-  await app.listen(process.env.PORT ?? 8080);
+    app.setGlobalPrefix('api')
+
+    app.use(bodyParser.json({ limit: '50mb' }))
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+
+    app.enableCors({
+        origin: ['http://localhost:3000', 'http://84.247.181.29:3000'],
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+        allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
+    })
+
+    await app.listen(process.env.PORT ?? 8080)
 }
 bootstrap()
